dda8128932c9a7afc9ccbebc7ccaba9a79a6a403
Borja Robles Fosg <broblesf@gmail.com>
2022-04-10 21:05:09 +0200

    Agregar archivos de proyecto.

---
+++ FruitsAsaitec/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace FruitsAsaitec
+{
+    partial class fruitShop
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculate = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.receipt = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(168, 24);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(87, 23);
+            this.calculate.TabIndex = 0;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.calculate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(423, 77);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.receipt);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(423, 422);
+            this.panel2.TabIndex = 3;
+            // 
+            // receipt
+            // 
+            this.receipt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.receipt.HideSelection = false;
+            this.receipt.Location = new System.Drawing.Point(0, 0);
+            this.receipt.MultiSelect = false;
+            this.receipt.Name = "receipt";
+            this.receipt.Size = new System.Drawing.Size(423, 422);
+            this.receipt.TabIndex = 0;
+            this.receipt.UseCompatibleStateImageBehavior = false;
+            // 
+            // fruitShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 499);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "fruitShop";
+            this.Text = "Fruit Shop";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView receipt;
+    }
+}
+
---
+++ FruitsAsaitec/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FruitsAsaitec
+{
+    public partial class fruitShop : Form
+    {
+        public fruitShop()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            //Declarations
+            List<productPrice> ProductPrice = new List<productPrice>();
+            List<purchase> Purchase = new List<purchase>();
+
+            string[] linesAux;
+
+            using (var writer = new System.IO.StreamWriter(Application.StartupPath + "\\Out.txt"))
+            {
+                //Console.SetOut(writer);
+            }
+
+                
+
+            try
+            {
+                //File D. configuration
+                openFileDialog.Multiselect = false;
+                openFileDialog.InitialDirectory = Application.ExecutablePath;
+
+                openFileDialog.Title = "[step 1/2] Select the price file (Product,Price)";
+                openFileDialog.ShowDialog(this);
+                //Process Product file
+                if (openFileDialog.FileName != "")
+                {
+                    linesAux = System.IO.File.ReadAllLines(openFileDialog.FileName);
+                    foreach (string x in linesAux.SkipWhile(b => b.StartsWith("PRODUCT")))
+                    {
+                        ProductPrice.Add(new productPrice
+                        {
+                            product = x.Split(',')[0],
+                            price = decimal.Parse(x.Split(',')[1].Replace("€", ""))
+                        });
+                    }
+                }
+                else
+                {
+                    throw new Exception("File not selected");
+                }
+
+                openFileDialog.Title = "[step 2/2] Select the purchase file (Product,Quantity)";
+                openFileDialog.ShowDialog(this);
+
+                //Process Purchase file
+                if (openFileDialog.FileName != "")
+                {
+                    linesAux = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+                    foreach (string x in linesAux.SkipWhile(b=> b.StartsWith("PRODUCT") ))
+                    {
+                        Purchase.Add(new purchase
+                        {
+                            item = x.Split(',')[0],
+                            quantity = decimal.Parse(x.Split(',')[1].Replace("€", ""))
+                        });
+                    }
+
+                }
+                else
+                {
+                    throw new Exception("File not selected");
+                }
+
+                List<offers> offer = new List<offers> { new offers {product= "Pear", gift= "", quantity= 0, amountToDiscount= 1, minimunQuantity= 4, } };
+
+                Calculate(ProductPrice, Purchase,offer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There are problems with the files, please verify the files and format files \r\n" + ex.Message );
+            }
+            Console.OpenStandardOutput();
+
+        }
+
+        void Calculate(List<productPrice> product, List<purchase> purchased, List<offers> offer)
+        {
+            //Group products
+            List<purchase> purchaseGroup = purchased.GroupBy(c => c.item).Select(g => new purchase { item = g.Key, quantity = g.Sum(a => a.quantity) }).ToList();
+
+            foreach (var item in purchaseGroup)
+            {
+                Console.WriteLine(item.item + "\t" + item.quantity.ToString());
+                System.IO.File.WriteAllText(Application.StartupPath + "\\OUT.txt", item.item + "\t" + item.quantity.ToString());
+                receipt.Items.Add(item.item + "\t" + item.quantity.ToString());
+            }
+
+            //DISCOUNTS
+            foreach (var item in purchaseGroup)
+            {
+                offers discount = offer.Find(c => c.product == item.item);
+                if (discount!= null)
+                {
+                    if (purchaseGroup.Find(x => x.item==discount.gift ).item !=null )
+                    {
+                        Console.WriteLine("DISCOUNT ***(Pending of completion)");
+                        System.IO.File.AppendAllText(Application.StartupPath + "\\OUT.txt", "DISCOUNT ***(Pending of completion)");
+                        receipt.Items.Add("DISCOUNT ***(Pending of completion)");
+
+                    }
+                }
+            }
+
+            //CALCULATE
+            Console.WriteLine("ITEMS \t AMOUNT ");
+            foreach (var item in purchaseGroup)
+            {
+                string txt = item.item + "\t" + (product.Find(c => c.product == item.item).price * item.quantity).ToString() + " €";
+                Console.WriteLine(txt);
+                System.IO.File.AppendAllText(Application.StartupPath + "\\OUT.txt", txt);
+                receipt.Items.Add(txt);
+
+            }
+
+            Console.WriteLine("TOTAL: ");
+            Console.WriteLine(purchaseGroup.Select(x => product.Find(c => c.product == x.item).price * x.quantity).Sum().ToString() + " €");
+            System.IO.File.AppendAllText(Application.StartupPath + "\\OUT.txt", purchaseGroup.Select(x => product.Find(c => c.product == x.item).price * x.quantity).Sum().ToString() + " €");
+            receipt.Items.Add("TOTAL");
+            System.IO.File.AppendAllText(Application.StartupPath + "\\OUT.txt", purchaseGroup.Select(x => product.Find(c => c.product == x.item).price * x.quantity).Sum().ToString() + " €");
+            receipt.Items.Add(purchaseGroup.Select(x => product.Find(c => c.product == x.item).price * x.quantity).Sum().ToString() + " €");
+
+
+
+
+
+
+        }
+    }
+
+    class offers{
+        public string product;
+        public string gift;
+        public decimal quantity;
+        public decimal amountToDiscount=0;
+        public decimal minimunQuantity = 0;
+
+    }
+
+
+    class productPrice
+    {
+        public string product;
+        public decimal price;
+    }
+
+    class purchase
+    {
+        public string item;
+        public decimal quantity;
+    }
+}
+
+
